f2cf0fba5fcfed4114890b1ab1f4ede83f242c2f
wooferzfg <spapushin@gmail.com>
2020-07-24 21:36:27 -0500

    Initial commit

---
+++ UI/Components/SplitInfo.cs
@@ -0,0 +1,42 @@
+﻿using LiveSplit.Model;
+using LiveSplit.TimeFormatters;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LiveSplit.UI.Components
+{
+    public class SplitInfo : LogicComponent
+    {
+        public override string ComponentName => "Split Info";
+
+        public SplitInfo()
+        {
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode) => null;
+
+        public override void SetSettings(System.Xml.XmlNode settings)
+        {
+        }
+
+        public override System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return document.CreateElement("SplitInfoSettings");
+        }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+
+        public int GetSettingsHashCode() => 1;
+    }
+}
---
+++ UI/Components/SplitInfoFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class SplitInfoFactory : IComponentFactory
+    {
+        public string ComponentName => "Split Info";
+
+        public string Description => "Outputs info about splits to a text file";
+
+        public ComponentCategory Category => ComponentCategory.Other;
+
+        public IComponent Create(LiveSplitState state) => new SplitInfo();
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/noupdates.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("1.0.0");
+    }
+}
